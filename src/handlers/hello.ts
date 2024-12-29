d58d834533b297862a7a1b812df4580f82c90416
vinodt31 <vinodt31@gmail.com>
2024-12-29 23:57:18 +0530

    changes commit

--- src/handlers/hello.ts
+++ src/handlers/hello.ts
@@ -1,5 +1,6 @@
 import { formatJSONResponse } from '../utils/response';
 
 export const handler = async () => {
+  //hello handler function
   return formatJSONResponse(200, { message: 'Hello, Serverless!' });
 };
